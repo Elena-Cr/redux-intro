085bdd16779d88eeaec760c100fe807f2cb3337b
Elena Cristescu <elenacristescu11@gmail.com>
2024-04-23 16:19:48 +0200

    build an action creater

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -13,7 +15,12 @@ function reducer(state = initialState, action) {
     case "account/requestLoan":
       if (state.loan > 0) return state;
       //LATER
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload.amount,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -25,3 +32,28 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+const store = createStore(reducer);
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, "Buy a cheap car"));
+store.dispatch(payLoan());
+console.log(store.getState());
